e6ee389965c0beda51cddc09614de131c9b0d847
Thiago MadPin <madpin@gmail.com>
2023-01-14 12:50:37 +0000

    Listing Saving

--- model/sqlmodel.py
+++ model/sqlmodel.py
@@ -13,8 +13,9 @@ from pydantic import BaseModel
 #     message: str
 #     chat_id: str = os.getenv('TG_ADMIN_CHAT_ID')
 #     timeout: int = 10
-#     
+#
 #     images: Optional[List[TelegramImageUrl]]
+
 
 class Message(BaseModel):
     title: str
@@ -23,29 +24,35 @@ class Message(BaseModel):
     to_: str
     method: str
 
+
 class Image(BaseModel):
     url: str
-    caption: str = ''
+    url_600: str
+    caption: str = ""
+
 
 class TelegramMessage(Message):
     to_: str = os.getenv("TG_ADMIN_CHAT_ID")
     disable_web_page_preview: bool = True
-    images: Optional[List[ImageUrl]]
+    images: Optional[List[Image]]
 
 
 class Point(BaseModel):
     lat: float
     long: float
 
+
 class Location(Point):
     name: str
     address: Optional[str]
     tags: Optional[List[str]]
 
+
 class InterestLocation(Location):
     website: Optional[str]
     website_domain: Optional[str]
     chain_name: Optional[str]
+
 
 class RouteSummary(BaseModel):
     walking_distance: int
@@ -56,29 +63,53 @@ class RouteSummary(BaseModel):
     from_: Optional[Point]
     to_: Optional[Point]
 
+
 # ########################################
 class Property(BaseModel):
-    location: Location
+    location: Optional[Location]
     bedrooms: str = None
     bathrooms: str = None
 
+
 class Listing(Property):
+    title: Optional[str]
+    listing_type: str = None  # Rent
+    property_type: str = None  # House, Apartment
+
     monthly_price: float
     url: str
-    title: Optional[str]
+    ber: str = None
+    description: str = None
+
+    facilities: List[str] = None
+
+    image_count: int = None
+    images: List[Image] = None
+    has_video: bool = None
+
+    hash_version: str = None
     publish_date: Optional[datetime.datetime]
+    updated_at: Optional[datetime.datetime]
+    views: str = 0
+
 
 class DaftListing(Listing):
-    category: str = None
+    premier_partner: str = None
     featured_level: str = None
+    daft_shortcode: str = None
+
     sections: List[str]
     source_code: str
+    overview: List[str] = None
+    has_virtual_tour: bool = None
+    has_brochure: bool = None
 
 
 class DaftSearchList(BaseModel):
     results_count: int
     search_rules: dict
     result_list: List[DaftListing]
+
 
 class DaftSearchItem(BaseModel):
     results_count: int
